20ade5f3ebc7b6dc3b4364ca455aecbcaabfbae4
Dharamraj <dpy9572@gmail.com>
2025-08-17 10:56:34 +0530

    Add root route

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,12 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.get("/", (req, res) => {
+  res.send("Hello Developer, Server is working");
+});
+
 app.use("/api/ai", aiRoutes);
 
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
